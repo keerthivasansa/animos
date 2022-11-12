42e745f6d7515eac55de6c6cd856fb2d46897ad9
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-11-12 12:28:39 +0530

    fixed process.env undefined

--- move.js
+++ move.js
@@ -1,7 +1,9 @@
 const { renameSync, rmSync } = require("fs");
 const { execSync } = require("child_process");
+const { config } = require("dotenv");
 
 let args = process.argv.slice(2);
+config();
 
 if (args[0] == "back") {
   rmSync("prisma/cache.db");
