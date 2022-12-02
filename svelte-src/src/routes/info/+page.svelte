269fce86ba57208023fc4f58a7da408f68567cc3
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-12-02 19:52:43 +0530

    info page responsive

--- svelte-src/src/routes/info/+page.svelte
+++ svelte-src/src/routes/info/+page.svelte
@@ -56,105 +56,111 @@
   Loading information
 {:then anime}
   {#if anime.available}
-    <div class="flex my-5 gap-36">
-      <img
-        src={anime.posterImg}
-        alt={getTitle(anime)}
-        class="w-80 rounded-lg"
-        style="height: 30rem;"
-      />
-      <div class="flex flex-col gap-6">
-        <h1 class="font-semibold text-3xl">{getTitle(anime)}</h1>
-        <Genres {anime} size="large" />
-        <!-- TODO add type -->
-        <div class="relative px-2">
-          <p
-            bind:this={synopsisElem}
-            class="mt-5 synopsis transition-all text-gray-200 ease-in-out duration-500"
-            class:limit-lines={!descriptionExpand}
-            style={descriptionExpand || !showExpand
-              ? "height: max-content; line-height: 1.8rem;"
-              : `height: ${maxDescription}px; line-height: 1.65rem`}
-          >
-            {anime.synopsis}
-          </p>
-          {#if showExpand && !descriptionExpand}
-            <div
-              class="absolute bottom-0 left-0 w-full h-full bg-black rounded-b-lg black-gradient"
-            />
-            <div
-              class="absolute cursor-pointer"
-              style="bottom: -1rem;left: calc(50% - 2rem)"
-              on:click={(_) => (descriptionExpand = true)}
+    <section>
+      <div
+        class="flex flex-col md:flex-row justify-center items-center my-5 gap-36"
+      >
+        <img
+          src={anime.posterImg}
+          alt={getTitle(anime)}
+          class="w-80 rounded-lg"
+          style="height: 30rem;"
+        />
+        <div class="flex flex-col gap-6">
+          <h1 class="font-semibold text-3xl">{getTitle(anime)}</h1>
+          <Genres {anime} size="large" />
+          <!-- TODO add type -->
+          <div class="relative px-2">
+            <p
+              bind:this={synopsisElem}
+              class="mt-5 synopsis transition-all text-gray-200 ease-in-out duration-500"
+              class:limit-lines={!descriptionExpand}
+              style={descriptionExpand || !showExpand
+                ? "height: max-content; line-height: 1.8rem;"
+                : `height: ${maxDescription}px; line-height: 1.65rem`}
             >
-              <IconBtn>
-                <FaDown />
-              </IconBtn>
-            </div>
-          {/if}
-        </div>
-        <div class="mt-10">
-          <div on:click={(_) => (relatedExpand = !relatedExpand)}>
-            <IconTxtBtn text={"Related"}>
-              {#if relatedExpand}
-                <FaUp />
-              {:else}
-                <FaDown />
-              {/if}
-            </IconTxtBtn>
-          </div>
-          {#if relatedExpand}
-            {#await getRelated() then related}
-              <div>
-                {#each Object.keys(related) as role}
-                  {#if related[role].length > 0}
-                    <h3 class="font-semibold my-6">{capitalize(role)}</h3>
-                    <div class="flex gap-10 flex-wrap w-full">
-                      {#each related[role] as relatedAnime}
-                        <CoverAnime anime={relatedAnime} infoOnHover={false} />
-                      {/each}
-                    </div>
-                  {/if}
-                {/each}
-              </div>
-            {/await}
-          {/if}
-        </div>
-        <div>
-          <div class="mt-14">
-            <span class="font-semibold text-xl mt-16">Episodes</span>
-            {#if anime.episodes > 100}
-              <select
-                class="bg-gray-200 h-fit w-fit text-sm ml-5 rounded-md"
-                bind:value={episodePage}
+              {anime.synopsis}
+            </p>
+            {#if showExpand && !descriptionExpand}
+              <div
+                class="absolute bottom-0 left-0 w-full h-full bg-black rounded-b-lg black-gradient"
+              />
+              <div
+                class="absolute cursor-pointer"
+                style="bottom: -1rem;left: calc(50% - 2rem)"
+                on:click={(_) => (descriptionExpand = true)}
               >
-                {#each Array.from( { length: Math.ceil(anime.episodes / 100) } ) as _, index}
-                  <option value={index}
-                    >{index * 100 + (anime.zeroEpisode ? 0 : 1)} - {(index +
-                      1) *
-                      100}</option
-                  >
-                {/each}
-              </select>
+                <IconBtn>
+                  <FaDown />
+                </IconBtn>
+              </div>
             {/if}
           </div>
-          <div
-            class="mt-10 flex gap-5 flex-wrap max-w-xl justify-center items-center"
-          >
-            {#each generateRange(episodePage * 100 + (anime.zeroEpisode ? 0 : 1), (episodePage + 1) * 100, anime.episodes) as epNo}
-              <a
-                style="width: {(anime.episodes.toString().length ?? 0) +
-                  1.25}rem;"
-                class=" bg-gray-300 text-center rounded-sm"
-                href="/episode?animeId={anime.kitsuId}&episodeId={epNo}&zeroEp={anime.zeroEpisode}&totalEpisode={anime.episodes}"
-              >
-                <span class="font-semibold text-black">{epNo}</span>
-              </a>
-            {/each}
+          <div class="mt-10">
+            <div on:click={(_) => (relatedExpand = !relatedExpand)}>
+              <IconTxtBtn text={"Related"}>
+                {#if relatedExpand}
+                  <FaUp />
+                {:else}
+                  <FaDown />
+                {/if}
+              </IconTxtBtn>
+            </div>
+            {#if relatedExpand}
+              {#await getRelated() then related}
+                <div>
+                  {#each Object.keys(related) as role}
+                    {#if related[role].length > 0}
+                      <h3 class="font-semibold my-6">{capitalize(role)}</h3>
+                      <div class="flex gap-10 flex-wrap w-full">
+                        {#each related[role] as relatedAnime}
+                          <CoverAnime
+                            anime={relatedAnime}
+                            infoOnHover={false}
+                          />
+                        {/each}
+                      </div>
+                    {/if}
+                  {/each}
+                </div>
+              {/await}
+            {/if}
           </div>
         </div>
       </div>
-    </div>
+      <div class="w-full grid place-items-center">
+        <div class="mt-14">
+          <span class="font-semibold text-xl mt-16">Episodes</span>
+          {#if anime.episodes > 100}
+            <select
+              class="bg-gray-200 h-fit w-fit text-sm ml-5 rounded-md"
+              bind:value={episodePage}
+            >
+              {#each Array.from( { length: Math.ceil(anime.episodes / 100) } ) as _, index}
+                <option value={index}
+                  >{index * 100 + (anime.zeroEpisode ? 0 : 1)} - {(index + 1) *
+                    100}</option
+                >
+              {/each}
+            </select>
+          {/if}
+        </div>
+        <div
+          class="mt-10 flex gap-5 flex-wrap max-w-xl justify-center items-center"
+        >
+          {#each generateRange(episodePage * 100 + (anime.zeroEpisode ? 0 : 1), (episodePage + 1) * 100, anime.episodes) as epNo}
+            <a
+              style="width: {(anime.episodes.toString().length ?? 0) +
+                1.25}rem;"
+              class=" bg-gray-300 text-center rounded-sm"
+              href="/episode?animeId={anime.kitsuId}&episodeId={epNo}&zeroEp={anime.zeroEpisode}&totalEpisode={anime.episodes}"
+            >
+              <span class="font-semibold text-black">{epNo}</span>
+            </a>
+          {/each}
+        </div>
+      </div>
+    </section>
     <div
       on:click={(_) => {
         anime.liked = !anime.liked;
