8e859e5212adaa5d2d47b84a95346dc52e029b38
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-12-04 11:00:28 +0530

    modal stores lock body on true

--- svelte-src/src/lib/stores.ts
+++ svelte-src/src/lib/stores.ts
@@ -1,9 +1,25 @@
-import { writable } from "svelte/store"
+import { writable, type Writable } from "svelte/store";
 
-export const showSettings = writable(false);
-
-export const accentClr = writable('');
+export const accentClr = writable("");
 
 export const coverAnimeFocus = writable(-1);
 
 export const lockBody = writable(false);
+
+export function createModal() {
+  let store = writable(false);
+  return {
+    set: (val: boolean) => {
+      if (val) {
+        lockBody.set(true);
+      } else {
+        lockBody.set(false);
+      }
+      return store.set(val);
+    },
+    subscribe: store.subscribe,
+    update: store.update,
+  };
+}
+
+export const showSettings = createModal();
