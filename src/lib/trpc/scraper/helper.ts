afbbf53422732a7d7197f4379a0dbd77cd89c2ba
Keerthi Vasan S A <sakeerthi23@gmail.com>
2023-02-12 10:53:39 +0530

    https proxy agent creation fixed

--- src/lib/trpc/scraper/helper.ts
+++ src/lib/trpc/scraper/helper.ts
@@ -1,12 +1,12 @@
 import { PROXY_AUTH, PROXY_IP, PROXY_PORT } from "$env/static/private";
 import axios from "axios";
-import HttpsProxyAgent from "https-proxy-agent/dist/agent";
+import HttpsProxyAgent from "https-proxy-agent";
 
 export const USER_AGENT = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36`;
 
 // for use with cloudflare
 export const proxyAxios = axios.create({
-    httpsAgent: new HttpsProxyAgent({
+    httpsAgent: HttpsProxyAgent({
         rejectUnauthorized: true,
         host: PROXY_IP,
         port: PROXY_PORT,
@@ -15,5 +15,5 @@ export const proxyAxios = axios.create({
     headers: {
         "User-Agent": USER_AGENT
     },
-    xsrfCookieName: "XSRF-TOKEN", 
+    xsrfCookieName: "XSRF-TOKEN",
 })
