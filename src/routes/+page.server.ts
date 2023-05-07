cdbb04bb944d97fc94b3f7c02bc562624b571838
Keerthi Vasan S A <sakeerthi23@gmail.com>
2023-05-07 19:31:31 +0530

    feat: prerender homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,7 @@
 import { AnimeService } from '@server/services/anime';
 
+export const prerender = true;
+
 export const load = async () => {
 	const trendingList = await AnimeService.getTrending();
 	const recommendations = await AnimeService.getGenre('Action');
